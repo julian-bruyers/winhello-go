65047c1bd8e285287b2e7503117dec07fc70e5b8
Julian Bruyers <julian.bruyers@gmail.com>
2026-01-05 20:10:01 +0100

    feat: Add winhello error definitions

---
+++ errors.go
@@ -0,0 +1,12 @@
+package winhello
+
+import "errors"
+
+var (
+	ErrOsNotSupported   = errors.New("winhello for Go is only available for Windows systems")
+	ErrArchNotSupported = errors.New("winhello is only supported for amd64 and ARM architectures")
+	ErrNotAvailable     = errors.New("Windows Hello is not available or configured on this system")
+	ErrUserCanceled     = errors.New("the user canceled the authentification")
+	ErrDLLLoad          = errors.New("failed to load dll")
+	ErrInternal         = errors.New("internal winhello error")
+)
